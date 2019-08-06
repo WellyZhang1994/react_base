ba9943422d6f4dcd3fc102b747ce3d329c27370b
WellyZhang1994 <ipt08110320@gmail.com>
2019-08-06 19:31:44 +0800

    rwd

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,55 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 
 class App extends React.Component{
-
+    
     render(){
+        console.log(this.props)
+        const { classes } = this.props;
         return (
-            <div className="ui container" style={{width:'100%',backgroundColor: '#12212c'}}>
-                hello
+            <div className={classes.root}>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} >
+                    <Paper className={classes.paper}>xs=12</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                    <Paper className={classes.paper}>xs=6</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                    <Paper className={classes.paper}>xs=6</Paper>
+                    </Grid>
+                    <Grid item xs={6} sm={3}>
+                    <Paper className={classes.paper}>xs=3</Paper>
+                    </Grid>
+                    <Grid item xs={6} sm={3}>
+                    <Paper className={classes.paper}>xs=3</Paper>
+                    </Grid>
+                    <Grid item xs={6} sm={3}>
+                    <Paper className={classes.paper}>xs=3</Paper>
+                    </Grid>
+                    <Grid item xs={6} sm={3}>
+                    <Paper className={classes.paper}>xs=3</Paper>
+                    </Grid>
+                </Grid>
             </div>
         );
     }
 };
 
-export default connect()(App);
+const useStyles = {
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: 20,
+      textAlign: 'center',
+      color: "red"
+    },
+  };
+
+
+export default connect()(withStyles(useStyles)(App));
